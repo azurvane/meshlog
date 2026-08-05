8f903451c7bf3037922d50dff8b3e2be844eb779
azurvane <179304994+azurvane@users.noreply.github.com>
2026-08-05 19:48:30 +0530

    implemented function to get table names and content
    extracting the names of the table from the sqlite file and getting the content of selected table via sending its name in the funciton

--- src-tauri/src/database/mod.rs
+++ src-tauri/src/database/mod.rs
@@ -1,6 +1,8 @@
 pub mod core;
 pub mod helper;
 
+pub use core::get_table_name;
+pub use core::get_table_entries;
 pub use core::get_new_asset_id;
 pub use core::view_new_asset_id;
 pub use core::update_db;
